dc4f73b8817967ab8df6a2b7026a15b5cba1d67d
danielcadengueorange <daniel.cadengue@orangedoor.com.br>
2020-07-07 15:12:15 -0300

    segundo commit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -262,7 +262,6 @@ export class DashboardComponent implements OnInit {
       }
     };
 
-
     var gradientBarChartConfiguration: any = {
       maintainAspectRatio: false,
       legend: {
@@ -311,41 +310,13 @@ export class DashboardComponent implements OnInit {
       }
     };
 
-    this.canvas = document.getElementById("chartLineRed");
-    this.ctx = this.canvas.getContext("2d");
+    
+  
 
-    var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
+  
 
-    gradientStroke.addColorStop(1, 'rgba(233,32,16,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
 
-    var data = {
-      labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-      datasets: [{
-        label: "Data",
-        fill: true,
-        backgroundColor: gradientStroke,
-        borderColor: '#ec250d',
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: '#ec250d',
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: '#ec250d',
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: [80, 100, 70, 80, 120, 80],
-      }]
-    };
-
-    var myChart = new Chart(this.ctx, {
-      type: 'line',
-      data: data,
-      options: gradientChartOptionsConfigurationWithTooltipRed
-    });
+   
 
 
     this.canvas = document.getElementById("chartLineGreen");
@@ -354,95 +325,17 @@ export class DashboardComponent implements OnInit {
 
     var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
 
-    gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
+    gradientStroke.addColorStop(1, 'rgba(66,134,121,0.20)');
     gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
     gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
 
-    var data = {
-      labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-      datasets: [{
-        label: "My First dataset",
-        fill: true,
-        backgroundColor: gradientStroke,
-        borderColor: '#00d6b4',
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: '#00d6b4',
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: '#00d6b4',
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: [90, 27, 60, 12, 80],
-      }]
-    };
-    
-
-    var myChart = new Chart(this.ctx, {
-      type: 'line',
-      data: data,
-      options: gradientChartOptionsConfigurationWithTooltipGreen
-
-    });
-
-
-
-    var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+    var chartbar1_labels = ['1', '2', '3', '4', '5'];
     this.datasets = [
-      [100, 70, 90, 70, 85, 60, 75, 60, 90, 80, 110, 100],
-      [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120],
-      [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130]
+      [2520, 5040, 7560, 10080, 12601],
+      [8838,30313 , 51789, 73264, 94741,],
     ];
-    this.data = this.datasets[0];
-
-
-
-    this.canvas = document.getElementById("chartBig1");
-    this.ctx = this.canvas.getContext("2d");
-
-    var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
-
-    gradientStroke.addColorStop(1, 'rgba(233,32,16,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
-
-    var config = {
-      type: 'line',
-      data: {
-        labels: chart_labels,
-        datasets: [{
-          label: "My First dataset",
-          fill: true,
-          backgroundColor: gradientStroke,
-          borderColor: '#ec250d',
-          borderWidth: 2,
-          borderDash: [],
-          borderDashOffset: 0.0,
-          pointBackgroundColor: '#ec250d',
-          pointBorderColor: 'rgba(255,255,255,0)',
-          pointHoverBackgroundColor: '#ec250d',
-          pointBorderWidth: 20,
-          pointHoverRadius: 4,
-          pointHoverBorderWidth: 15,
-          pointRadius: 4,
-          data: this.data,
-        }]
-      },
-      options: gradientChartOptionsConfigurationWithTooltipRed
-    };
-    this.myChartData = new Chart(this.ctx, config);
-
-
-    this.canvas = document.getElementById("CountryChart");
-    this.ctx  = this.canvas.getContext("2d");
-    var gradientStroke = this.ctx.createLinearGradient(0, 230, 0, 50);
-
-    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
-
+    this.data = this.datasets[1];
+      
 
     var myChart = new Chart(this.ctx, {
       type: 'bar',
@@ -451,9 +344,133 @@ export class DashboardComponent implements OnInit {
         display: false
       },
       data: {
-        labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+        labels: chartbar1_labels,
         datasets: [{
-          label: "Countries",
+          label: "Lucro presumido",
+          fill: true,
+          backgroundColor: 'rgba(23,251,133,0.20)',
+          hoverBackgroundColor: gradientStroke,
+          borderColor: '#17FB85',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 0.0,
+          data: this.datasets[0],
+          
+          
+        },
+        {
+          label: "Simples adicional",
+          fill: true,
+          backgroundColor: 'rgba(251,133,23,0.20)',
+          borderColor: '#FB8517',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 1.0,
+          pointBackgroundColor: '#ffff00',
+          pointBorderColor: 'rgba(255,255,255,)',
+          pointHoverBackgroundColor: '#ffff00',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: this.data,
+          
+        }
+        ]
+      },
+      options: gradientBarChartConfiguration
+    });
+
+
+    var chart_labels = ['1', '2', '3', '4', '5'];
+    this.datasets = [
+      [4531, 15461, 26390, 37320, 48249],
+      [911, 911, 911, 911, 911,],
+    ];
+
+
+    this.data = this.datasets[1];
+
+    this.canvas = document.getElementById("chartBig1");
+    this.ctx = this.canvas.getContext("2d");
+
+    var gradientStroke = this.ctx.createLinearGradient(0, 300, 0, 10);
+
+    gradientStroke.addColorStop(1, 'rgba(233,32,16,1.0)');
+    gradientStroke.addColorStop(0.4, 'rgba(233,32,16,0.0)');
+    gradientStroke.addColorStop(0, 'rgba(233,32,16,0)'); //red colors
+
+    var config = {
+      type: 'line',
+      data: {
+        labels: chart_labels,
+        datasets: [{
+          label: "Economia acumulada",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: '#ec250d',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 1.0,
+          pointBackgroundColor: '#ec250d',
+          pointBorderColor: 'rgba(255,255,255,0)',
+          pointHoverBackgroundColor: '#ec250d',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: this.datasets[0],
+        },
+        {
+          label: "Economia mensal",
+          fill: true,
+          backgroundColor: gradientStroke,
+          borderColor: '#ffff00',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 1.0,
+          pointBackgroundColor: '#ffff00',
+          pointBorderColor: 'rgba(255,255,255,0)',
+          pointHoverBackgroundColor: '#ffff00',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: this.data,
+        }
+        ]
+      },
+      options: gradientChartOptionsConfigurationWithTooltipRed
+    };
+    this.myChartData = new Chart(this.ctx, config);
+
+
+    this.canvas = document.getElementById("CountryChart");
+    this.ctx  = this.canvas.getContext("2d");
+    var gradientStroke = this.ctx.createLinearGradient(10, 300, 10, 200);
+
+    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.20)');
+    gradientStroke.addColorStop(0.4, 'rgba(251,247,54,0.20)');
+    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+
+
+    var chartbar_labels = ['1', '2', '3', '4', '5'];
+    this.datasets = [
+      [6897, 13794, 20691, 27588, 34851],
+      [7167, 14875, 22583, 30291, 37999,],
+    ];
+    this.data = this.datasets[1];
+
+    var myChart = new Chart(this.ctx, {
+      type: 'bar',
+      responsive: true,
+      legend: {
+        display: false
+      },
+      data: {
+        labels: chartbar_labels,
+        datasets: [{
+          label: "Lucro presumido",
           fill: true,
           backgroundColor: gradientStroke,
           hoverBackgroundColor: gradientStroke,
@@ -461,8 +478,26 @@ export class DashboardComponent implements OnInit {
           borderWidth: 2,
           borderDash: [],
           borderDashOffset: 0.0,
-          data: [53, 20, 10, 80, 100, 45],
-        }]
+          data: this.datasets[0],
+        },
+        {
+          label: "Simples adicional",
+          fill: true,
+          backgroundColor: 'rgba(251,247,54,0.20)',
+          borderColor: '#ffff00',
+          borderWidth: 2,
+          borderDash: [],
+          borderDashOffset: 1.0,
+          pointBackgroundColor: '#ffff00',
+          pointBorderColor: 'rgba(255,255,255,)',
+          pointHoverBackgroundColor: '#ffff00',
+          pointBorderWidth: 20,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 15,
+          pointRadius: 4,
+          data: this.data,
+        }
+        ]
       },
       options: gradientBarChartConfiguration
     });
